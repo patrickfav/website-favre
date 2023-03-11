efd313a9fe607b620931619915a64a79ebdb5504
Patrick Favre-Bulle <patrick@favre.at>
2023-03-11 14:45:12 +0100

    Fix actually updating content in cron ci job

--- content-downloader/src/cli.js
+++ content-downloader/src/cli.js
@@ -4,14 +4,27 @@ import { downloadMediumArticles } from './downloader/medium'
 import { downloadStackOverflowPosts } from './downloader/stackoverflow'
 import { downloadGists } from './downloader/gist'
 
-export function cli () {
-  const rootDirMd = '../content/'
+const defaultRootDir = '../content/'
+
+export function cli (args) {
+
+  const rootDir = parseArguments(args)
+
   const relOutDirGithub = 'opensource/'
   const relOutDirArticles = 'articles/'
 
-  downloadGists(githubProjectsUser, gistIds, rootDirMd, relOutDirGithub)
-    .then(() => downloadGithubProjects(githubProjectsUser, githubProjects, rootDirMd, relOutDirGithub))
-    .then(() => downloadStackOverflowPosts(stackoverflowUserId, rootDirMd, relOutDirArticles))
-    .then(() => downloadMediumArticles(rootDirMd, relOutDirArticles))
+  downloadGists(githubProjectsUser, gistIds, rootDir, relOutDirGithub)
+    .then(() => downloadGithubProjects(githubProjectsUser, githubProjects, rootDir, relOutDirGithub))
+    .then(() => downloadStackOverflowPosts(stackoverflowUserId, rootDir, relOutDirArticles))
+    .then(() => downloadMediumArticles(rootDir, relOutDirArticles))
     .then(() => console.log('Waiting to finish'))
+}
+
+function parseArguments(args) {
+
+  if(args && args.length && args.length >= 3) {
+    return args[2];
+  }
+
+  return defaultRootDir;
 }
