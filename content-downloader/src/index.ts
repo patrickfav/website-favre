d5d067d9d6ccb61610eb974174653674e07aa60f
Patrick Favre-Bulle <patrick@favre.at>
2023-03-18 17:43:11 +0100

    Fix build & gitinfo

--- content-downloader/src/index.ts
+++ content-downloader/src/index.ts
@@ -7,6 +7,8 @@ console.log(`
   / __/__ __  ______  / ___/___/ _ \\
  / _// _ \`/ |/ / __/ / /__/___/ // /
 /_/  \\_,_/|___/_/    \\___/   /____/                            
+
+Starting at ${new Date().toISOString()}
 `)
 
 require = esm(module /*, options*/);
